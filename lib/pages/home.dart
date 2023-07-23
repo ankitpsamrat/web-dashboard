0bf62101728bab53d6b57813103e35557d8043d9
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-07-23 23:45:29 +0530

    file added

---
+++ lib/pages/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:web_dashboard/utils/constants.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    w = MediaQuery.of(context).size.width;
+    h = MediaQuery.of(context).size.height;
+
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // NavBar(),
+            SizedBox(height: 100),
+            // Container1(),
+            // Container2(),
+            // Container3(),
+            // Container4(),
+            // Container5()
+
+            // MainContent()
+          ],
+        ),
+      ),
+    );
+  }
+}
